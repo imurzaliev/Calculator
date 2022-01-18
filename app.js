dd6fce0e629f9a11aa166676baa7e1b1f4922274
imurzaliev <imurzalievi@gmial.com>
2022-01-18 11:21:54 +0300

    Create an operate function

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const add = (arr) => {
   return arr.reduce((total, currentNum) => (total += currentNum), 0);
 };
-console.log(add([2, 4, 3, 1]));
 
 const substract = (arr) => {
   return arr.reduce((total, currentNum) => (total -= currentNum));
 };
-console.log(substract([2, 4, 3, 1]));
 
 const multiply = (arr) => {
   return arr.length
     ? arr.reduce((accumulator, nextNum) => accumulator * nextNum)
     : 0;
 };
-console.log(multiply([2, 2, 2]));
 
 const divide = (arr) => {
   return arr.length
     ? arr.reduce((accumulator, nextNum) => accumulator / nextNum)
     : 0;
 };
-console.log(divide([2, 2, 2]));
+
+const operate = (operator, numbers) => {
+  return operator(numbers);
+};
+console.log(operate(add, [2, 4, 3, 1]));
